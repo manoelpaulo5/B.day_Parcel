13fc412ff807c57a66694cd2e98382a1fdc97876
Maneol Paulo <paulo.neto.jp@hotmail.com>
2025-03-20 14:51:54 -0300

    project finish

---
+++ src/scripts/main.js
@@ -0,0 +1,29 @@
+AOS.init();
+
+const dataDoEvento = new Date("sep 30, 2025 18:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento= Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+
+    document.getElementById('timer').innerHTML = `Começa em: ${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+    if (horasAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+
+        document.getElementById('timer').innerHTML = 'Até o próximo Aniversário';
+    }
+
+}, 1000);
